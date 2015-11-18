73ac3d687e570a84624c953805df64bb9cf87714
Oded Welgreen <oded_wel@hotmail.com>
2015-11-18 20:26:30 +0200

    Add columns every x seconds

--- Main.elm
+++ Main.elm
@@ -5,11 +5,18 @@ import Keyboard
 import Time exposing (..)
 import Window
 import Debug
+import Array
+import List
 
 (gameWidth,gameHeight) = (800,480)
 
 type State = Play | Start | GameOver
 
+type alias Column =
+  { x : Float
+  , bottomHeight: Float
+  , topHeight: Float
+  }
 type alias Game =
   { state : State
   , foregroundX : Float
@@ -17,6 +24,7 @@ type alias Game =
   , y : Float
   , vy : Float
   , timeToColumn : Float
+  , columns : Array.Array Column
   }
 
 type alias KeyUpdate =
@@ -43,14 +51,14 @@ defaultGame =
   , y = 0
   , vy = 0
   , timeToColumn = constants.timeBetweenColumns
+  , columns = Array.empty
   }
 
 -- UPDATE
 update : Input -> Game -> Game
 update input game =
   let
-    newGame =
-      Debug.watch "game" game
+    newGame = Debug.watch "game" game
   in
     case input of
       TimeDelta delta ->
@@ -99,16 +107,33 @@ applyPhysics delta game =
 
 updateColumns : TimeUpdate
 updateColumns delta game =
-    {game | timeToColumn <-
+  let
+    timeToColumn =
       if | game.timeToColumn <= 0 -> constants.timeBetweenColumns
          | game.state == Play -> game.timeToColumn - delta
          | otherwise -> game.timeToColumn
+    shouldAddColumn = timeToColumn == constants.timeBetweenColumns && game.state == Play
+    updatedColumns =
+      Array.map (\c -> {c | x <- c.x - constants.foregroundScrollV * delta}) game.columns
+    columns =
+      if | game.state /= Play -> game.columns
+         | shouldAddColumn ->
+            updatedColumns |>
+              Array.push {
+                x = gameWidth
+                , bottomHeight = 0
+                , topHeight = 0
+              }
+         | otherwise -> updatedColumns
+  in
+    {game | timeToColumn <- timeToColumn
+          , columns <- columns
     }
 
 --Input updates
 transitionState : KeyUpdate
 transitionState space game =
-  if game.state == GameOver && space then defaultGame
+  if game.state == GameOver && space then defaultGame --Reset
   else
     {game |
       state <-
@@ -131,21 +156,25 @@ view (w,h) game =
       if game.state == GameOver then 1 else 0
     getReadyAlpha =
       if game.state == Start then 1 else 0
+    columnForms =
+      Array.map (\c -> toForm(image 108 239 "/images/rock.png") |> move (c.x, 0)) game.columns
+    formList =
+      [
+         toForm (image gameWidth gameHeight "/images/background.png")
+           |> move (-game.backgroundX, 0)
+      ,  toForm (image gameWidth gameHeight "/images/background.png")
+           |> move (gameWidth - game.backgroundX, 0)
+      ,  toForm (image 60 35 "/images/plane.gif")
+          |> move (constants.playerX, game.y)
+      ,  toForm (image 400 70 "/images/textGameOver.png")
+          |> alpha gameOverAlpha
+      ,  toForm (image 400 70 "/images/textGetReady.png")
+              |> alpha getReadyAlpha
+      ]
   in
     container w h middle <|
-    collage gameWidth gameHeight
-     [
-        toForm (image gameWidth gameHeight "/images/background.png")
-          |> move (-game.backgroundX, 0)
-     ,  toForm (image gameWidth gameHeight "/images/background.png")
-          |> move (gameWidth - game.backgroundX, 0)
-     ,  toForm (image 60 35 "/images/plane.gif")
-         |> move (constants.playerX, game.y)
-     ,  toForm (image 400 70 "/images/textGameOver.png")
-         |> alpha gameOverAlpha
-     ,  toForm (image 400 70 "/images/textGetReady.png")
-             |> alpha getReadyAlpha
-     ]
+    collage gameWidth gameHeight formList
+      -- (List.append (Array.toList columnForms) formList)
 
 -- SIGNALS
 type Input =
